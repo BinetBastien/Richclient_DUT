8b7624d834365498d1de0d2febcb098e6e18fe69
BinetBastien <binet.bastien@live.fr>
2016-02-02 10:06:46 +0100

    AjoutTotal
    Mise à jour du total et du nombre de services sélectionnés

--- TD1_2/js/controller.js
+++ TD1_2/js/controller.js
@@ -18,9 +18,8 @@ angular.module("Services").controller("controllerServices", [function(){
       active : false
     }
   ];
-
+  this.nbServs = 0;
   this.services = service;
-
   this.change = function(service){
     if(service.active==true){
       service.active=false;
@@ -29,5 +28,21 @@ angular.module("Services").controller("controllerServices", [function(){
       service.active=true;
     }
   }
+
+  this.calcTotal = function(){
+    var taille = this.services.length;
+    var total=0;
+    var i=0;
+    var nb = 0;
+    for(i=0;i<taille;i++){
+      if(this.services[i].active==true){
+        total+= this.services[i].price;
+        nb++;
+      }
+    }
+    this.nbServs = nb;
+    return total;
+  }
+
 
 }]);
